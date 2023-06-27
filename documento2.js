03e3a54d636b6501e5651b9d2da8a19cceb3cae4
Sonqo-Toconas <Sonqo-Toconas>
2023-06-27 11:26:46 -0300

    Agregue a documento2

--- documento2.js
+++ documento2.js
@@ -2,9 +2,13 @@
     {
         mascota: perro,
         edad: 2,
-     },
+    },
     {
         mascota: gato,
         edad: 2,
-      }
+    },
+    {
+        mascota: Capibara,
+        edad: 10
+    }
 ]
